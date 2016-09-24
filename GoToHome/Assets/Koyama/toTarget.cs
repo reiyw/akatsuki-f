c2c0c088f28b4a71f15aa514edb5b8e23aff9581
Ryu <m0114173b0@edu.teu.ac.jp>
2016-09-24 15:15:13 +0900

    NavMeshを利用して経路探索

---
+++ GoToHome/Assets/Koyama/toTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toTarget : MonoBehaviour {
+    public GameObject target;
+    private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.transform.position);
+        //transform.Translate(target.transform.position - transform.position);
+	}
+}
